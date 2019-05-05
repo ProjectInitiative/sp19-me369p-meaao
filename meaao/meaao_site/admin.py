23b5be6e49180a34e51951ab3916c577b0644285
Andrew Gu <1711729+a-gu@users.noreply.github.com>
2019-05-05 16:27:13 -0500

    Initial Routing and Database
    - Added basic routing to static string response
    - Updated models and migrations for database

--- meaao/meaao_site/admin.py
+++ meaao/meaao_site/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Walkin, Contact
+
+
+admin.site.register(Walkin)
+admin.site.register(Contact)
--- meaao/meaao_site/models.py
+++ meaao/meaao_site/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Walkin(models.Model):
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='advisee', on_delete=models.PROTECT)
+    advisor_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='advisor', on_delete=models.PROTECT)
+    order = models.IntegerField()
+    comments = models.TextField()
+
+
+class Contact(models.Model):
+    recipient = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=255)
+    user_eid = models.CharField(max_length=255)
+    user_email = models.CharField(max_length=255)
+    message = models.TextField()
