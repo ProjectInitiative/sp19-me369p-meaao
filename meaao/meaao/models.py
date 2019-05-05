f36b24d1d80a92f8a6f4ab365fb6de353d21156b
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2019-05-04 23:08:52 -0500

    Removed some field restrictions

--- meaao/meaao/models.py
+++ meaao/meaao/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-    
+
 class  Walkins(models.Model):
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT)
     advisor_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT)
@@ -9,8 +9,8 @@ class  Walkins(models.Model):
     comments = models.TextField()
 
 class Contact(models.Model):
-    recipient = models.CharField(max_length = 255)
-    user_name = models.CharField(max_length = 255)
+    recipient = models.CharField()
+    user_name = models.CharField()
     user_eid = models.CharField(max_length = 255)
-    user_email = models.CharField(max_length = 255)
+    user_email = models.CharField()
     message = models.TextField()
